f0170154abe641264d43e93ddd980c1c44339dcb
Alexander Myagkov <anistonbale@gmail.com>
2022-06-17 14:36:47 +0300

    create 2 array

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 ﻿/*Написать программу,которая из имеющегося массива строк формирует массив из строк,длина которых меньше либо
 равна 3 символа.Первоначальный массив можно ввести с клавиатуры,либо задать на старте выполнения алгоритма.*/
 
+Console.WriteLine("Введите количество используемых строк: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayOriginal = new string[size];
+string[] arrayAnswer=new string[size];
+
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
