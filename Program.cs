0754d1acc441fda2907988e505d95ed4495fc06e
Alexander Myagkov <anistonbale@gmail.com>
2022-06-17 14:37:36 +0300

    create circle

--- Program.cs
+++ Program.cs
@@ -5,6 +5,19 @@ Console.WriteLine("Введите количество используемых 
 int size = Convert.ToInt32(Console.ReadLine());
 string[] arrayOriginal = new string[size];
 string[] arrayAnswer=new string[size];
+for (int i = 0; i < arrayOriginal.Length; i++)
+{
+    Console.WriteLine($"Введите {i+1} строку");
+    arrayOriginal[i] = Console.ReadLine()+' ';
+    if (arrayOriginal[i].Length<5)
+    {
+        arrayAnswer[i]=arrayOriginal[i];
+    }
+}
+Console.Write("Ваш массив: ");
+PrintArray(arrayOriginal);
+Console.WriteLine("Ответ:");
+PrintArray(arrayAnswer);
 
 void PrintArray(string[] array)
 {
